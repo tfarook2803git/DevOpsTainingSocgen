c6d7573d105d651de12c0e9c6d7525c461d2600d
Thahseen Farook <farookthahseen@gmail.com>
2026-02-11 14:22:18 +0530

    creating the ec2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,16 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.web_server.id
+  value       = aws_instance.example.id
 }
 
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web_server.public_ip
+  value       = aws_instance.example.public_ip
 }
 
 output "instance_private_ip" {
   description = "Private IP address of the EC2 instance"
-  value       = aws_instance.web_server.private_ip
+  value       = aws_instance.example.private_ip
 }
 
 
--- terraform/resource.tf
+++ terraform/resource.tf
@@ -1,5 +1,5 @@
 # EC2 Instance Resource
-resource "aws_instance" "web_server" {
+resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   count         = var.instance_count
