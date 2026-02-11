fa151fff642ee6c26cbb549f5fdcef654f96f1ce
Thahseen Farook <farookthahseen@gmail.com>
2026-02-11 14:25:07 +0530

    creating the ec2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,12 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.example.id
+  value       = aws_instance.example.*.id[0]
 }
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.example.*.id[1]
+}
+
 
 
