6e14539f70011d1df05ae3ca9c9ddf4f67d797f6
Thahseen Farook <farookthahseen@gmail.com>
2026-02-12 17:08:50 +0530

    modified

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,7 +1,6 @@
 resource "aws_ecr_repository" "my_ecr" {
   name = "farook-ecr-repo"
   image_tag_mutability = "MUTABLE"
-  region = var.aws_region
 
    image_scanning_configuration {
     scan_on_push = true
